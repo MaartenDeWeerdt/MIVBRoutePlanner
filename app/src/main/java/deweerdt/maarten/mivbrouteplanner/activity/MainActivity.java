d7a41f6a0f5c0579ecc32eac0bffabc8e8990d8e
Maarten De Weerdt <maarten.dw@hotmail.be>
2017-05-09 10:26:18 +0200

    Final Setup

--- app/src/main/java/deweerdt/maarten/mivbrouteplanner/activity/MainActivity.java
+++ app/src/main/java/deweerdt/maarten/mivbrouteplanner/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package deweerdt.maarten.mivbrouteplanner;
+package deweerdt.maarten.mivbrouteplanner.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import deweerdt.maarten.mivbrouteplanner.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/deweerdt/maarten/mivbrouteplanner/model/Halte.java
@@ -0,0 +1,10 @@
+package deweerdt.maarten.mivbrouteplanner.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Maarten De Weerdt on 9/05/2017.
+ */
+
+public class Halte implements Serializable {
+}
