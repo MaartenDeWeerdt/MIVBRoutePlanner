659167ebadd8c38fb401d1887091acc9de132e70
Maarten De Weerdt <maarten.dw@hotmail.be>
2017-05-11 10:59:38 +0200

    mainactivity ok

--- app/src/main/java/deweerdt/maarten/mivbrouteplanner/activity/MainActivity.java
+++ app/src/main/java/deweerdt/maarten/mivbrouteplanner/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import deweerdt.maarten.mivbrouteplanner.R;
+import deweerdt.maarten.mivbrouteplanner.fragments.StopsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new StopsFragment()).commit();
+        }
 
     }
 
